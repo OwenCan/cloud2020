6d30534da44d6d6ad4b06a53f6f4d3b6c303771e
owen.zhao <18510672440@163.com>
2021-02-02 23:07:50 +0800

    配置eureka集群

---
+++ cloud-eureka-server7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @description: 服务注册中心2
+ * @author: Owen Zhao
+ * @create: 2021-02-02 22:02
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
