51e8eb66c29736362dd8a35448beb03162826a1d
OwenZhao <760141714@qq.com>
2021-02-07 14:15:18 +0800

    Hystrix-Dashboard面板

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/HystrixDashboardMain9001.java
@@ -0,0 +1,20 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @program: cloud2020
+ * @description: Hystrixx可视化面板
+ * @author: Owen Zhao
+ * @create: 2021-02-07 09:34
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
